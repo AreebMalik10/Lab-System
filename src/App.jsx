84d73de4f5b6c74c070450cde60894bf4beafdd3
Areeb Malik <areeb9273@gmail.com>
2026-04-16 22:42:24 +0500

    updated with project setup with material ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
-import Header from './components/Header'
+import Header from './components/Header'   //header component
 import Home from './screens/Home/Home'
 import Login from './screens/Auth/Login'
 import Dashboard from './screens/Dashboard/Dashboard'
@@ -15,7 +15,7 @@ export default function App(){
   const { user } = useAuth()
 
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>     
       <CssBaseline />
       <BrowserRouter>
         <Header />
